c7dd48cca8034cefceb302ea73c0e28d0d4388fc
Monirul Hasan <kmhasan@seu.edu.bd>
2018-10-25 13:23:28 +0600

    updates

--- src/main/java/bd/edu/seu/nosqldemo/AppUserForm.java
+++ src/main/java/bd/edu/seu/nosqldemo/AppUserForm.java
@@ -2,6 +2,9 @@ package bd.edu.seu.nosqldemo;
 
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.converter.StringToIntegerConverter;
+import com.vaadin.flow.data.validator.EmailValidator;
 
 public class AppUserForm extends FormLayout {
     private TextField idField;
@@ -9,11 +12,40 @@ public class AppUserForm extends FormLayout {
     private TextField emailField;
     private TextField phoneField;
 
+    private Binder<AppUser> appUserBinder;
+
     public AppUserForm() {
         idField = new TextField("ID");
         nameField = new TextField("Name");
         emailField = new TextField("Email");
         phoneField = new TextField("Phone");
         add(idField, nameField, emailField, phoneField);
+
+        appUserBinder = new Binder<>();
+
+        appUserBinder
+                .forField(nameField)
+                .asRequired()
+                .bind(AppUser::getName, AppUser::setName);
+
+        appUserBinder
+                .forField(emailField)
+                .withValidator(new EmailValidator("Must enter valid email address"))
+                .bind(AppUser::getEmail, AppUser::setEmail);
+
+        appUserBinder
+                .forField(phoneField)
+                .bind(AppUser::getPhone, AppUser::setPhone);
+
+        appUserBinder
+                .forField(idField)
+                .withConverter(new StringToIntegerConverter("Must enter a number"))
+                .withValidator(integer -> integer >= 1000 && integer <= 9999, "Must be a 4 digit number")
+                .bind(AppUser::getUserId, AppUser::setUserId);
+
+    }
+
+    public Binder<AppUser> getAppUserBinder() {
+        return appUserBinder;
     }
 }
--- src/main/java/bd/edu/seu/nosqldemo/AppUserView.java
+++ src/main/java/bd/edu/seu/nosqldemo/AppUserView.java
@@ -1,14 +1,33 @@
 package bd.edu.seu.nosqldemo;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route("")
 public class AppUserView extends VerticalLayout {
     private AppUserForm appUserForm;
+    private Button submitButton;
 
-    public AppUserView() {
+    private AppUser appUser;
+
+    private AppUserRepository appUserRepository;
+
+    public AppUserView(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+
+        appUser = new AppUser();
+
         appUserForm = new AppUserForm();
-        add(appUserForm);
+        submitButton = new Button("Submit");
+        add(appUserForm, submitButton);
+
+        submitButton.addClickListener(buttonClickEvent -> {
+            appUserForm.getAppUserBinder().writeBeanIfValid(appUser);
+            Notification.show("Saving " + appUser.getName());
+            appUserRepository.save(appUser);
+            Notification.show("Saved " + appUser.getName());
+        });
     }
 }
